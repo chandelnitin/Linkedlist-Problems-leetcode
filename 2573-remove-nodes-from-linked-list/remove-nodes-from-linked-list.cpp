647fc2367178acdaa73f0700f170678ef6c83867
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-05-09 00:32:17 +0530

    Time: 28 ms (19.80%) | Memory: 172 MB (40.48%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -8,30 +8,68 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
-public:  // maine isme stack approch use ki hai jb bhi koi element stack me pde element se bada hota
- // hai to mai stack ke top ko pop krta rahta hu jab tak stack ke top pr greater element nhi milta
- // ab mai mai stack me pade lement se ek list bnata hu lekin prblm ye hai stack pr top element list ka end element hona chaiye first nhi isliye maine reverse linked lis bnai.  dusra trika normal 
- // mai stack ke top ement ko first element bhi bna sakta tha or fir final list ko reverse kr deta .
- // jaise array se list bnao or fir us list ko revrse kr do
+// class Solution {  // o(n) sc O(n)
+// public:  // maine isme stack approch use ki hai jb bhi koi element stack me pde element se bada hota
+//  // hai to mai stack ke top ko pop krta rahta hu jab tak stack ke top pr greater element nhi milta
+//  // ab mai mai stack me pade lement se ek list bnata hu lekin prblm ye hai stack pr top element list ka end element hona chaiye first nhi isliye maine reverse linked lis bnai.  dusra trika normal 
+//  // mai stack ke top ement ko first element bhi bna sakta tha or fir final list ko reverse kr deta .
+//  // jaise array se list bnao or fir us list ko revrse kr do
+//     ListNode* removeNodes(ListNode* head) {
+//         ListNode* mover=head;
+//         stack<int>st;
+//         while(mover){
+//            while(!st.empty() && st.top()<mover->val){
+//                st.pop();
+//            }
+//            st.push(mover->val);
+//            mover=mover->next;
+//         }
+//         ListNode* temp=new ListNode(st.top()) ;
+//         temp->next=NULL;
+//         st.pop();
+//         while(!st.empty()){
+//             ListNode* newNode =new ListNode(st.top()) ;
+//             newNode->next=temp;
+//             temp=newNode;
+//             st.pop();
+//         }
+
+//         return temp;
+//     }
+// };
+
+
+
+
+
+
+
+class Solution {  // o(n) sc O(n)
+public:   // approch hmm list me last traverse krnege jiske liye hme list ko revrse krna pdega o
+// last se traverse krte time hmm max elemnt strore krte chalnge agr aage aane wale element max se chota hai iska matlab curr element ke right me ysse bada ekement hai jaise  13 3 8 list me 8 maximum hai suru me.(last element hmesa aayega ouptput me kuki sabse last elemnt ke right me koi greater element nhi hai). ab hmm 3 pr aayenge or chek krenge ki 3 max se bada hai kya nhi hai to 3 output me nhi aayega kuki max element hmm 3 ke right se hi le raha hai . or agr hme max elelt se bada element milta hai to max ko update krenge or reverse list create krenge .
     ListNode* removeNodes(ListNode* head) {
-        ListNode* mover=head;
-        stack<int>st;
-        while(mover){
-           while(!st.empty() && st.top()<mover->val){
-               st.pop();
-           }
-           st.push(mover->val);
-           mover=mover->next;
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        while(curr){  // linked list reverse ho gyi ab new head prev hoga
+             ListNode* front = curr->next;
+             curr->next=prev;
+             prev=curr;
+             curr=front;
         }
-        ListNode* temp=new ListNode(st.top()) ;
+        // prev head hai ab list ka 
+        int maxelment=prev->val;
+        ListNode* temp= new ListNode(prev->val);
+        prev=prev->next;
         temp->next=NULL;
-        st.pop();
-        while(!st.empty()){
-            ListNode* newNode =new ListNode(st.top()) ;
-            newNode->next=temp;
-            temp=newNode;
-            st.pop();
+        while(prev){   
+            if(prev->val>=maxelment){
+               ListNode* newNode= new ListNode(prev->val);
+               newNode->next=temp;
+               temp=newNode;
+                maxelment=prev->val;
+            }
+
+            prev=prev->next;
         }
 
         return temp;
