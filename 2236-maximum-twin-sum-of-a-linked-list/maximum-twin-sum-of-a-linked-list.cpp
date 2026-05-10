c9aa06a334eca6d3bd9978a99a9214643929ec11
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-05-10 16:25:46 +0530

    Time: 13 ms (8.59%) | Memory: 134 MB (30.44%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -1,3 +1,53 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public: // twin frist and last honge list ke fir second or list ke second last isliye deque me strore kr kiye sare element kuki deque me front or back dono ka access hota hai
+//     int pairSum(ListNode* head) {
+//         deque<int>dq;
+//         ListNode* temp=head;
+//         while(temp){
+//             dq.push_back(temp->val);
+//             temp=temp->next;
+//         }
+//         int sum=0;
+//         int Max=INT_MIN;
+//         while(!dq.empty()){
+//            sum=dq.front()+dq.back();
+//            dq.pop_front();
+//            dq.pop_back();
+//            Max=max(sum,Max);
+//         }
+
+//         return Max;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,23 +59,26 @@
  * };
  */
 class Solution {
-public: // twin frist and last honge list ke fir second or list ke second last isliye deque me strore kr kiye sare element kuki deque me front or back dono ka access hota hai
+public: 
     int pairSum(ListNode* head) {
-        deque<int>dq;
+        stack<int>st;
         ListNode* temp=head;
         while(temp){
-            dq.push_back(temp->val);
+            st.push(temp->val);
             temp=temp->next;
         }
-        int sum=0;
-        int Max=INT_MIN;
-        while(!dq.empty()){
-           sum=dq.front()+dq.back();
-           dq.pop_front();
-           dq.pop_back();
-           Max=max(sum,Max);
-        }
 
-        return Max;
+        temp=head;
+        int sum=0; int n=st.size();
+        int maxSum=INT_MIN;
+        for(int i=0;i<n/2;i++){
+            sum=temp->val+st.top();
+            st.pop();
+            maxSum=max(maxSum,sum);
+            temp=temp->next;
+        }
+        
+
+        return maxSum;
     }
 };
