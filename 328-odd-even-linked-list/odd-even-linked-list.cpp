4ee462bb15911d0c01d8db275f8e5d161b62e158
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-05-13 00:03:18 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (27.21%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -1,3 +1,54 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public: o(N) SC O(n)
+//     ListNode* oddEvenList(ListNode* head) {
+//         vector<int>nums;
+//         ListNode* temp=head;
+//         while(temp){
+//             nums.push_back(temp->val);
+//             temp=temp->next;
+//         }
+//         temp=head;
+//         for(int i=0;i<nums.size();i=i+2){
+//             temp->val=nums[i];
+//             temp=temp->next;
+//         }
+
+//         for(int i=1;i<nums.size();i=i+2){
+//             temp->val=nums[i];
+//             temp=temp->next;
+//         }
+
+//         return head;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -11,23 +62,31 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        vector<int>nums;
+        if(head ==NULL || head->next==NULL) return head;
         ListNode* temp=head;
+        ListNode* odd=head;
+        ListNode* oddprev=temp;
+        temp=temp->next;
+        ListNode* even=temp;
+        ListNode* evenprev=temp;
+        temp=temp->next;
+        int count=3;
         while(temp){
-            nums.push_back(temp->val);
-            temp=temp->next;
-        }
-        temp=head;
-        for(int i=0;i<nums.size();i=i+2){
-            temp->val=nums[i];
-            temp=temp->next;
-        }
+           if(count%2==0){
+              evenprev->next=temp;
+              evenprev=temp;
+           } 
+           else{
+              oddprev->next=temp;
+              oddprev=temp;
+           }
 
-        for(int i=1;i<nums.size();i=i+2){
-            temp->val=nums[i];
-            temp=temp->next;
+           temp=temp->next;
+           count++;
         }
-
-        return head;
+        evenprev->next=NULL;
+        oddprev->next=even;
+      
+        return odd;
     }
 };
