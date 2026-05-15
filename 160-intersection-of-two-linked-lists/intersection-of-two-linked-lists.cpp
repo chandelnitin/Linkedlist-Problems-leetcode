ff9511963564e60986e8b6947ab1cc2368bad3da
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-05-16 02:13:56 +0530

    Time: 57 ms (54.84%) | Memory: 23.9 MB (84.29%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,3 +1,40 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//            unordered_set<ListNode*>st;
+//            ListNode* temp1=headA;
+//            while(temp1){
+//               st.insert(temp1);   // store every node of tem1
+//               temp1=temp1->next;
+//            }
+
+//            ListNode* temp2= headB;
+//            while(temp2){
+//               if(st.find(temp2)!=st.end()){ // agr phale se node exist krti hai to vo hi 
+//                   return temp2;// intersection node hai;
+//               }
+//               temp2=temp2->next;
+//            }
+
+
+//            return NULL;
+//     }
+// };
+
+
+
+
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,22 +46,26 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-           unordered_set<ListNode*>st;
-           ListNode* temp1=headA;
-           while(temp1){
-              st.insert(temp1);   // store every node of tem1
-              temp1=temp1->next;
-           }
+          ListNode* a=headA;
+          ListNode* b=headB;
 
-           ListNode* temp2= headB;
-           while(temp2){
-              if(st.find(temp2)!=st.end()){ // agr phale se node exist krti hai to vo hi 
-                  return temp2;// intersection node hai;
-              }
-              temp2=temp2->next;
-           }
+          while(a!=b){
+             if(a==NULL){
+                a=headB;
+             }
+             else{
+                a=a->next;
+             }
 
 
-           return NULL;
+             if(b==NULL){
+                b=headA;
+             }
+             else{
+                b=b->next;
+             }
+          }
+
+          return a;
     }
 };
