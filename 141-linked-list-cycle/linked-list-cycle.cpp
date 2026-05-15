f081bee8e4a1fc643fbe42a85b31669334cc73b8
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-05-16 03:44:24 +0530

    Time: 8 ms (83.58%) | Memory: 11.9 MB (51.90%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -1,3 +1,35 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         ListNode* temp=head;
+//         unordered_set<ListNode*>st;
+//         while(temp){
+//             if(st.find(temp)!=st.end()){
+//                 return true;
+//             }
+//             st.insert(temp);
+//             temp=temp->next;
+//         }
+
+//         return false;
+//     }
+// };
+
+
+
+
+
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,16 +41,15 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* temp=head;
-        unordered_set<ListNode*>st;
-        while(temp){
-            if(st.find(temp)!=st.end()){
-                return true;
-            }
-            st.insert(temp);
-            temp=temp->next;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast && fast->next){
+            // if(fast==slow) return true; // ye condition glat hai yhha kuki suru me to
+            fast=fast->next->next;// fast and slow same node pr honge na
+            slow=slow->next;
+            if(fast==slow) return true;
         }
-
+        
         return false;
     }
 };
