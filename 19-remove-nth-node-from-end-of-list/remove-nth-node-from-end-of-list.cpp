98f79f80141d46e5831d40845511bc33b9f17186
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-05-19 20:25:25 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (24.33%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,3 +1,65 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public: // tc (2n)  but agli approch thodi clean hai
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         ListNode* temp = head;
+//         int len=0;
+        
+//         while(temp){
+//             len++;
+//             temp=temp->next;
+//         }
+//         if(n==len){   // delete first node
+//             head=head->next;
+//             return head;
+//         }
+
+        
+//         int kth=len-n; // delete node from the starting +1
+//         temp=head;
+//         ListNode* prev=NULL;
+//         int count=0;
+//         while(temp){
+//             if(count==kth){
+//                 prev->next=temp->next;
+//                 delete temp;
+//                 break;
+//             }
+
+//             count++;
+//             prev=temp;
+//             temp=temp->next;
+//         }
+        
+//         return head;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -26,17 +88,16 @@ public:
         
         int kth=len-n; // delete node from the starting +1
         temp=head;
-        ListNode* prev=NULL;
-        int count=0;
+        int count=1;
         while(temp){
             if(count==kth){
-                prev->next=temp->next;
-                delete temp;
+                ListNode* deltenode= temp->next;
+                temp->next=temp->next->next;
+                delete deltenode;
                 break;
             }
 
             count++;
-            prev=temp;
             temp=temp->next;
         }
         
