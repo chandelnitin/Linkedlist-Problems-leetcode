1be6fa96d811132d78cdbee48533e13282f9722b
chandelnitin <128496532+chandelnitin@users.noreply.github.com>
2026-05-19 21:34:39 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (92.22%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -60,6 +60,55 @@
 
 
 
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode() : val(0), next(nullptr) {}
+//  *     ListNode(int x) : val(x), next(nullptr) {}
+//  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         ListNode* temp = head;
+//         int len=0;
+        
+//         while(temp){
+//             len++;
+//             temp=temp->next;
+//         }
+//         if(n==len){   // delete first node
+//             head=head->next;
+//             return head;
+//         }
+
+        
+//         int kth=len-n; // delete node from the starting +1
+//         temp=head;
+//         int count=1;
+//         while(temp){
+//             if(count==kth){
+//                 ListNode* deltenode= temp->next;
+//                 temp->next=temp->next->next;
+//                 delete deltenode;
+//                 break;
+//             }
+
+//             count++;
+//             temp=temp->next;
+//         }
+        
+//         return head;
+//     }
+// };
+
+
+
+
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -73,34 +122,24 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* temp = head;
-        int len=0;
-        
-        while(temp){
-            len++;
-            temp=temp->next;
+        ListNode* slow = head;
+         ListNode* fast = head;
+        for(int i=0;i<n;i++){
+            fast=fast->next;
         }
-        if(n==len){   // delete first node
+        if(fast==NULL){
+            ListNode* deleteNode= head;
             head=head->next;
+            delete deleteNode;
             return head;
         }
-
-        
-        int kth=len-n; // delete node from the starting +1
-        temp=head;
-        int count=1;
-        while(temp){
-            if(count==kth){
-                ListNode* deltenode= temp->next;
-                temp->next=temp->next->next;
-                delete deltenode;
-                break;
-            }
-
-            count++;
-            temp=temp->next;
+        while(fast->next){
+            slow=slow->next;
+            fast=fast->next;
         }
-        
+        ListNode* deleteNode=slow->next;
+        slow->next=slow->next->next;
+        delete deleteNode;
         return head;
     }
 };
